a78479caef18f1b9797e92e5faddb37bb2c3ca7d
Konstantin Modebadze <niriddiki@gmail.com>
2017-07-13 21:09:00 +0300

    d

--- sqrt.go
+++ sqrt.go
@@ -5,13 +5,13 @@ import (
 )
 
 func Sqrt(x float64) float64 {
-	z:=1.0
-	for i:=0; i<10; i++ {
-		z=z-((z*z-x)/(2*z))
+	z := 1.0
+	for i := 0; i < 10; i++ {
+		z = z - ((z*z - x) / (2 * z))
 	}
 	return z
 }
 
 func main() {
-	fmt.Println(Sqrt(3))
+	fmt.Println(Sqrt(152))
 }
